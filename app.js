daa01275f574a2a8a1f7704bd53ab263f961c227
Dinindu98 <37993810+Dinindu98@users.noreply.github.com>
2019-07-02 19:10:40 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
-const fname ='your';
-const lname = 'dsd';
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const  clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
 
-let abc;
-abc = ` 
-<h1> ${fname} </h1>
-`;
-//document.body.innerHTML = abc;
+loadEventListners();
+
+function loadEventListners(){
+
+    form.addEventListener('submit',addTask);
+}
+
+function addTask(e){
+    if(taskInput.value === ''){
+        alert('Add a task');
+    }
+
+    const li = document.createElement('li');
+    li.className = 'collection-item';
+    li.appendChild(document.createTextNode(taskInput.value));
+    const link = document.createElement('a');
+    link.className = 'delete-item secondary-content';
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    li.appendChild(link);
+    console.log(li);
+    e.preventDefault();
+}
